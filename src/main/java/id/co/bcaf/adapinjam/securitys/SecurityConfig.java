8148a483df324c01274171eb7347d8c2291db978
aryaganteng <hadi.argaya@gmail.com>
2025-05-30 16:49:37 +0700

    Update vercel 2

--- src/main/java/id/co/bcaf/adapinjam/securitys/SecurityConfig.java
+++ src/main/java/id/co/bcaf/adapinjam/securitys/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://adapinjam.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
